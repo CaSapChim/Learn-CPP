299fdbe2c87e2fbb58861e1441c51a31fa0bab6f
Kry <117553015+KrySoSad@users.noreply.github.com>
2023-04-16 12:40:30 +0700

    Create Arithmetic-Operators..cpp

---
+++ Arithmetic-Operators..cpp
@@ -0,0 +1,17 @@
+int score = 0;
+// score is 0
+ 
+score = 4 + 2;
+// it is now 6
+ 
+score = 4 - 2;
+// it is now 2
+ 
+score = 4 * 2;
+// it is now 8
+ 
+score = 4 / 2;
+// and now 2
+ 
+score = 5 % 2;
+// and now 1
