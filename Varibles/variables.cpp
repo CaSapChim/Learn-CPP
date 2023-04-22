35d34c893a631e1acd14c9f1b27227d9e369ff31
Kry <117553015+KrySoSad@users.noreply.github.com>
2023-04-22 11:53:54 +0700

    Update variables.cpp

--- Varibles/variables.cpp
+++ Varibles/variables.cpp
@@ -1,9 +1,11 @@
-int age = 28;
+int age = 28; // Khai báo biến age có kiểu dữ liệu số nguyên 
+
+float f; // Khai báo biến f có kiểu dữ liệu số thực
  
-double price = 8.99;
+double price = 8.99; // Khai báo biến price có kiểu dữ liệu số thập phân 
  
-char grade = 'A';
+char grade = 'A'; // Khai báo biến grade có kiểu dữ liệu ký tự 
  
 std::string message = "Game Over";
  
-bool late_to_work = true;
+bool late_to_work = true; // Kiểu dữ liệu logic
