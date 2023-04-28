75d7bfef70af1372273d6c98777157522d207abf
Kry <117553015+KrySoSad@users.noreply.github.com>
2023-04-28 17:02:11 +0700

    câu lệnh if-else

---
+++ Điều-kiện/If.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main() {
+  
+  int a = 5;
+  int b = 6;
+  
+  if ( a > b ) {
+      cout << "a lớn hơn b";
+  } else if (a < b) {
+      cout << "a bé hơn b";
+  } else {
+      cout << "a bằng b";
+  }
+
+}
