01c930ced5c9592352d630515b1f12db55f3aa2c
Kry <117553015+KrySoSad@users.noreply.github.com>
2023-04-28 17:07:48 +0700

    cấu trúc switch-case

---
+++ Điều-kiện/switch-case.cpp
@@ -0,0 +1,74 @@
+int month;
+    cout << "Tháng: " << endl;
+    cin >> month;
+
+    switch (month)
+    {
+        case 1:
+        {
+            cout << "Tháng 1" << endl;
+            break;
+        }
+        case 2:
+        {
+            cout << "Tháng 2" << endl;
+            break;
+        }
+        case 3:
+        {
+            cout << "Tháng 3" << endl;
+            break;
+        }
+        case 4:
+        {
+            cout << "Tháng 4" << endl;
+            break;
+        }
+        case 5:
+        {
+            cout << "Tháng 5" << endl;
+            break;
+        }
+        case 6:
+        {
+            cout << "Tháng 6" << endl;
+            break;
+        }
+        case 7:
+        {
+            cout << "Tháng 7" <<endl;
+            break;
+        }
+        case 8:
+        {
+            cout << "Tháng 8" << endl;
+            break;
+        }
+        case 9:
+        {
+            cout << "Tháng 9" << endl;
+            break;
+        }
+        case 10:
+        {
+            cout << "Tháng 10" << endl;
+            break;
+        }
+        case 11:
+        {
+            cout << "Tháng 11" << endl;
+            break;
+        }
+        case 12:
+        {
+            cout << "Tháng 12" << endl;
+            break;
+        }
+        default:
+        {
+            cout << "Nhập sai" << endl;
+        }
+    }
+
+    return 0;
+}
