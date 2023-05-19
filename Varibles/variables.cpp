ff1b7132455dfb9ec846948d358233eb56a6d1d0
Kry <117553015+KrySoSad@users.noreply.github.com>
2023-05-19 22:43:45 +0700

    Update variables.cpp

--- Varibles/variables.cpp
+++ Varibles/variables.cpp
@@ -9,6 +9,17 @@ char grade = 'A'; // Khai báo biến grade có kiểu dữ liệu ký tự
  
 std::string message = "Game Over";
  
-bool late_to_work = true; // Kiểu dữ liệu logic
+bool late_to_work = true; // Kiểu dữ liệu logic 
 
 string name = "Kry"; // Khai báo kiểu string (chuỗi)
+
+
+/*
+Miền giá trị của kiểu short là từ -32768 tới 32767.
+
+Miền giá trị của kiểu int là từ -2147483648 tới 2147483647.
+
+Miền giá trị của kiểu long là từ -2147483647 tới 2147483647 (giống với miền dữ liệu của kiểu int).
+
+Miền giá trị của kiểu long long là từ -9223372036854775808 tới 9223372036854775807
+*/
